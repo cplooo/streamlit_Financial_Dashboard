1f790e06061d701abe6065f9d6040411203fe3f2
cplooo <cplo@kimo.com>
2024-01-11 11:46:56 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub.py
@@ -20,7 +20,9 @@ stc.html(html_temp)
 
 #df = pd.read_excel("kbars_台積電_1100701_1100708_2.xlsx")
 #df = pd.read_excel("kbars_2330_2022-07-01-2022-07-31.xlsx")
-df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
+#df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
+## 读取Pickle文件
+df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
 df_original = df_original.drop('Unnamed: 0',axis=1)
 #df.columns  ## Index(['time', 'open', 'low', 'high', 'close', 'volume', 'amount'], dtype='object')
